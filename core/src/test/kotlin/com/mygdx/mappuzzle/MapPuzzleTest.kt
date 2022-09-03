68f144df2b56143b04e1003e5b59a0bf1be3a8b2
non-emulated-programmer <60372726+non-emulated-programmer@users.noreply.github.com>
2022-09-03 16:03:43 +1200

    fixed project structure, added test sources root for unit tests and a test file in it, need to fix junit dependencies

---
+++ core/src/test/kotlin/com/mygdx/mappuzzle/MapPuzzleTest.kt
@@ -0,0 +1,62 @@
+package com.mygdx.mappuzzle
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MapPuzzleTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    fun setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    fun tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getBatch() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setBatch() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getAssetManager() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setAssetManager() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getColors() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setColors() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getLevels() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setLevels() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun create() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun createLevels() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun render() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun dispose() {
+    }
+}
