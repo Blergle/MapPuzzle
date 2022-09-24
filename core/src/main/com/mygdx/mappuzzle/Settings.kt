ee7d8ea8280053c7605e87103207af77e20ba2e1
non-emulated-programmer <60372726+non-emulated-programmer@users.noreply.github.com>
2022-09-25 03:59:35 +1300

    Finally got a unit test to work. This required a small rewrite of the setLevel() function of the Settings class, which should be copied in its other functions. The SharedPreferences component of Settings will have to separately be tested through an instrumented test.

--- core/src/main/com/mygdx/mappuzzle/Settings.kt
+++ core/src/main/com/mygdx/mappuzzle/Settings.kt
@@ -3,12 +3,24 @@ package com.mygdx.mappuzzle
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Preferences
 import kotlin.properties.Delegates
+import kotlin.reflect.typeOf
 
 open class Settings {
     var settings: Preferences = Gdx.app.getPreferences("My Settings") //for persisting settings after app closure
-    var currentLevel by Delegates.notNull<Int>() //set the level to be played - shouldn't ever be set directly, but kept as class variable for quicker access
+    open var currentLevel by Delegates.notNull<Int>() //set the level to be played - shouldn't ever be set directly, but kept as class variable for quicker access
+    //open var currentLevel: Int = 0
     private var notifications by Delegates.notNull<Boolean>() //example setting we could add: daily notifications enabled or not  - shouldn't ever be set directly, but kept as class variable for quicker access
     private var random by Delegates.notNull<Boolean>()
+
+
+    //stores data persistently - should probably refactor so all storing functions here call this, for easier unit testing
+    open fun store(key: String, data: Any){
+        when(data){
+            is Boolean -> settings.putBoolean(key, data)
+            is Int -> settings.putInteger(key, data)
+        }
+        settings.flush()
+    }
 
     //example setting - allow or disallow notifications
     fun allowNotifications(hasNotifications: Boolean) {
@@ -22,11 +34,12 @@ open class Settings {
         return settings.getBoolean("Notifications", false)
     }
 
-    //set which entry in the levels list is to be played
-    fun setLevel(level: Int){
+    //set which entry in the levels list is to be played - this has been set up to be easily unit tested
+    open fun setLevel(level: Int){
         currentLevel = level
-        settings.putInteger("Level", currentLevel)
-        settings.flush()
+        store("Level", level)
+        //settings.putInteger("Level", currentLevel)
+        //settings.flush()
     }
 
     //get the index in the levels list of the current level - safer than accessing class variable
--- core/src/test/kotlin/com/mygdx/mappuzzle/SettingsTest.kt
+++ core/src/test/kotlin/com/mygdx/mappuzzle/SettingsTest.kt
@@ -1,21 +1,30 @@
 package com.mygdx.mappuzzle
 
-import org.junit.Assert.*
-
 import org.junit.After
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Captor
 import org.mockito.Mockito
 import org.mockito.Mockito.*
+import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.*
 
-class SettingsTest {
+//@RunWith(MockitoJUnitRunner::class)
+class SettingsTest { //this is probably better done as an instrumented test, since Settings accesses SharedPreferences for most functions
+
+//    @Captor
+    lateinit var intCaptor: KArgumentCaptor<Int>
+//    @Captor
+    lateinit var stringCaptor: KArgumentCaptor<String>
 
     private lateinit var settings: Settings
 
     @Before
     fun setUp() {
-        settings = mock()
+        settings = mock() //can't use spy(), but I want to check invocation of functions called within functions :(
+        //settings = Settings()
     }
 
     @After
@@ -48,7 +57,26 @@ class SettingsTest {
     }
 
     @Test
-    fun setLevel() {
+    fun setLevelTest() {
+        //val mock: MapPuzzle = spy()
+        //settings = mock<Settings>{
+        //    on {setLevel(1)}
+        //}
+        //mock.settings = settings
+        //mock.settings.settings = Gdx.app.getPreferences("My Settings")
+        //mock.settings.currentLevel = 0
+        //mock.settings.setLevel(1)
+        //Mockito.verify(mock.settings.settings).putInteger("Level", 1)
+        //settings.settings = spy()
+        //settings.currentLevel = 0
+        intCaptor = argumentCaptor<Int>()
+        stringCaptor = argumentCaptor<String>()
+        whenever(settings.setLevel(anyInt())).thenCallRealMethod()
+        settings.setLevel(1)
+        Mockito.verify(settings).store(stringCaptor.capture(), intCaptor.capture())
+        assertEquals("Level", stringCaptor.firstValue)
+        assertEquals(1, intCaptor.firstValue)
+        //Mockito.verify(settings).store(anyString(), anyInt())
     }
 
     @Test
